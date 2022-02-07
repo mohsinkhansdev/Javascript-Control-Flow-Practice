e9ce0e18b8b5ced4a96fad29ebb65e922d88c951
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-07 18:53:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+
+
+
+// JavaScript Basics Control Flow
+
+/* console.log("Print All numbers divisible by 5 and 3 between 5 and 50");
+for (var i = 5; i < 51; i++) {
+if(i % 5 === 0 && i % 3 === 0) {
+    console.log(i)
+}
+} */
+
+/* console.log("Print All odd numbers between 300 and 333");
+for (var i = 300; i < 333; i+=3) {
+console.log(i);
+} */
+
+/* console.log("Print All even numbers between 10 and 40");
+for (var i = 10; i < 41; i+=2) {
+console.log(i);
+} */
+
+/* console.log("Print All numbers between -10 and 19");
+for (var i = -10; i < 20; i++) {
+console.log(i);
+} */
+
+/* var str = "abcdefghif";
+
+for(var i = 1; i < str.length; i+=2){
+    console.log(str[i])
+} */
+
+/* for(var i = 0; i < 16; i+=8){
+    console.log(i);
+} */
+
+/* var str = "hello";
+for(var i = 0; i < str.length; i++){
+    console.log(str[i]);
+} */
+/* for (var count = 0; count <= 5; count++; ){
+console.log(count);
+} */
+
+/* var answer = prompt("Are we there Yet?");
+
+while (answer !== "yes" && answer !== "yeah") {
+    var answer = prompt("are we there yet?");
+}
+
+alert("Yay, we made it"); */
+
+/* var answer = prompt("Are we there Yet?");
+
+while (answer.indexOf("yes") === -1 ) {
+    var answer = prompt("are we there yet?");
+}
+
+alert("Yay, we made it"); */
+
+/* var count = -10;
+console.log("Print All numbers between -10 and 19" )
+
+while (count < 20) {
+console.log(count)
+count++;
+}
+
+var count = 10;
+console.log("Print All Even Numbers Between 10 and 40" )
+
+while (count <= 40 ) {
+console.log(count)
+count+=2;
+}
+
+var count = 300;
+console.log("Print All Even Numbers Between 10 and 40" )
+
+while (count <= 333 ) {
+console.log(count)
+count+=3;
+}
+
+var count = 5;
+console.log("Print All Numbers divisable by 5 and 3 between 5 and 50" )
+
+while (count <= 50 ) {
+    if(count % 5 === 0 && count % 3 === 0){
+        console.log(count)
+    }
+count+=1;
+} */
